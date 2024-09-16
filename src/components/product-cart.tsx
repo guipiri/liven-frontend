227f19aa541d91672321f19f4b1fc318d9504b20
xuxa <gui.soliveiras@gmail.com>
2024-09-16 09:27:11 -0300

    refactor: prodct in cart separated from cart page in a new component

---
+++ src/components/product-cart.tsx
@@ -0,0 +1,69 @@
+import { useContext } from 'react'
+import { FaMinusSquare, FaPlusSquare } from 'react-icons/fa'
+import { IoClose } from 'react-icons/io5'
+import { CartContext } from '../contexts/cart-provider'
+import { ProductInCart } from '../types/cart'
+
+export default function ProductCart({ prod }: { prod: ProductInCart }) {
+  const { addToCart, removeOneFromCart, removeFromCart } =
+    useContext(CartContext)
+
+  return (
+    <div
+      key={prod.id}
+      className="flex h-48 sm:h-60 w-full max-w-xl items-center border-b mb-4"
+    >
+      <span className="relative bottom-24 right-0 hover:cursor-pointer">
+        <IoClose
+          onClick={() => {
+            removeFromCart(prod)
+          }}
+          size={22}
+        />
+      </span>
+      <div className="shrink-0 h-48 sm:h-60 flex justify-center items-center mr-4 sm:mr-6">
+        <img
+          src={prod.image}
+          alt={prod.title}
+          className=" size-28 object-contain sm:size-48"
+        />
+      </div>
+      <div className="w-full overflow-hidden flex flex-col justify-around py-6 h-48 sm:h-60">
+        <h2 className="text-lg font-bold truncate">{prod.title}</h2>
+        <div className="flex justify-between">
+          <p className="text-lg">
+            {prod.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </p>
+          <div className="flex items-center text-xl w-16 justify-between self-center">
+            <span>
+              <FaPlusSquare
+                onClick={() => {
+                  addToCart(prod)
+                }}
+                className="rounded-md fill-green-800 hover:cursor-pointer"
+              />
+            </span>
+            {prod.qty}
+            <span>
+              <FaMinusSquare
+                onClick={() => {
+                  removeOneFromCart(prod)
+                }}
+                className="rounded-md fill-red-800 hover:cursor-pointer"
+              />
+            </span>
+          </div>
+        </div>
+        <span className="text-2xl text-end">
+          {(prod.qty * prod.price).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </span>
+      </div>
+    </div>
+  )
+}
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,11 +1,9 @@
 import { useContext } from 'react'
-import { FaMinusSquare, FaPlusSquare } from 'react-icons/fa'
-import { IoClose } from 'react-icons/io5'
+import ProductCart from '../components/product-cart'
 import { CartContext } from '../contexts/cart-provider'
 
 export default function Cart() {
-  const { cart, addToCart, removeOneFromCart, removeFromCart } =
-    useContext(CartContext)
+  const { cart } = useContext(CartContext)
 
   return (
     <div className="flex items-center flex-col px-6 py-10">
@@ -14,64 +12,7 @@ export default function Cart() {
       )}
       {cart?.map((prod) => {
         if (prod.qty === 0) return <></>
-        return (
-          <div
-            key={prod.id}
-            className="flex h-48 sm:h-60 w-full max-w-xl items-center border-b mb-4"
-          >
-            <span className="relative bottom-24 right-0 hover:cursor-pointer">
-              <IoClose
-                onClick={() => {
-                  removeFromCart(prod)
-                }}
-                size={22}
-              />
-            </span>
-            <div className="shrink-0 h-48 sm:h-60 flex justify-center items-center mr-4 sm:mr-6">
-              <img
-                src={prod.image}
-                alt={prod.title}
-                className=" size-28 object-contain sm:size-48"
-              />
-            </div>
-            <div className="w-full overflow-hidden flex flex-col justify-around py-6 h-48 sm:h-60">
-              <h2 className="text-lg font-bold truncate">{prod.title}</h2>
-              <div className="flex justify-between">
-                <p className="text-lg">
-                  {prod.price.toLocaleString('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  })}
-                </p>
-                <div className="flex items-center text-xl w-16 justify-between self-center">
-                  <span>
-                    <FaPlusSquare
-                      onClick={() => {
-                        addToCart(prod)
-                      }}
-                      className="rounded-md fill-green-800 hover:cursor-pointer"
-                    />
-                  </span>
-                  {prod.qty}
-                  <span>
-                    <FaMinusSquare
-                      onClick={() => {
-                        removeOneFromCart(prod)
-                      }}
-                      className="rounded-md fill-red-800 hover:cursor-pointer"
-                    />
-                  </span>
-                </div>
-              </div>
-              <span className="text-2xl text-end">
-                {(prod.qty * prod.price).toLocaleString('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                })}
-              </span>
-            </div>
-          </div>
-        )
+        return <ProductCart prod={prod} />
       })}
       <h2 className="text-2xl font-bold flex justify-between w-full max-w-xl">
         <span className="">Total: </span>
